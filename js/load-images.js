b10860938e9169d519cd2d615c4e413ee1f64b6a
Mr. David J. Acevedo II <19176326+SavageAvocado1@users.noreply.github.com>
2018-05-09 23:37:07 -0400

    Create load-images.js

---
+++ js/load-images.js
@@ -0,0 +1,19 @@
+var loadingImages = true;
+var imageIndex = 0;
+    
+do {
+      
+  var url = "https://cdn.cornhubs.gq/images/cornhubs_home_" + this.imageIndex + ".jpg";
+        
+  var http = new XMLHttpRequest();
+  http.open('HEAD', url, false);
+  http.send();
+  
+  if (http.status != 404)
+    document.getElementById("content").innerHTML += "<img src=\"" + url + "\" alt=\"corn\"></br>";
+  else
+    break;
+        
+  imageIndex++;
+      
+} while (loadingImages);
