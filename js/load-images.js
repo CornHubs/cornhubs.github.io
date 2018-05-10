20d9f55e0be1b0e5733c428741e0fa5473c2ad26
Mr. David J. Acevedo II <19176326+SavageAvocado1@users.noreply.github.com>
2018-05-10 16:25:12 -0400

    Update load-images.js

--- js/load-images.js
+++ js/load-images.js
@@ -10,7 +10,7 @@ do {
   http.send();
   
   if (http.status != 404)
-    document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\"></br>";
+    document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\" style=\"padding-top: 20px; display: block; margin: auto; width: 40%;\"></br>";
   else
     break;
         
