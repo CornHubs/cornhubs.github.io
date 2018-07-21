c4de2bc76c97e207761ba1ec62fa1a717cedb658
Mr. David J. Acevedo II <19176326+SavageAvocado1@users.noreply.github.com>
2018-07-21 13:22:09 -0400

    Update load-images.js

--- js/load-images.js
+++ js/load-images.js
@@ -6,7 +6,7 @@ do {
   var url = "https://cdn.cornhubs.gq/images/cornhubs_home_" + this.imageIndex + ".jpg";
         
   var http = new XMLHttpRequest();
-  http.open('HEAD', url, false);
+  http.open('HEAD', url, true);
   http.send();
   
   if (http.status != 404)
