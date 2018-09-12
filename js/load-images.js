19911fe4e860796334fd09d8bf6bd627f4ce25c9
David <acevedo72799@gmail.com>
2018-09-12 17:06:53 -0400

    Test.

--- js/load-images.js
+++ js/load-images.js
@@ -1,19 +1,15 @@
-var loadingImages = true;
-var imageIndex = 0;
-    
+imageIndex = 0;
+
+let http = new XMLHttpRequest();
+
 do {
-      
-  var url = "https://cdn.cornhubs.gq/images/cornhubs_home_" + this.imageIndex + ".jpg";
-        
-  var http = new XMLHttpRequest();
-  http.open('GET', url, true);
-  http.send();
-  
-  if (http.status != 404)
-    document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\" style=\"padding-top: 20px; display: block; margin: auto; width: 40%;\"></br>";
-  else
-    break;
-        
-  imageIndex++;
-      
-} while (loadingImages);
+    let url = "https://cdn.cornhubs.gq/images/cornhubs_home_" + imageIndex + ".jpg";
+
+    http.open('GET', url, false);
+    http.send();
+
+    if (http.status !== 404)
+        document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\" style=\"padding-top: 20px; display: block; margin: auto; width: 40%;\" /><br>";
+
+    imageIndex++;
+} while (http.status !== 404);
