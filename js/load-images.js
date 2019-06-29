036c6400a099f6a28d343182a00c7043c7d01e68
Mr. David J. Acevedo II <19176326+SavageAvocado@users.noreply.github.com>
2019-06-28 23:37:34 -0400

    Update load-images.js

--- js/load-images.js
+++ js/load-images.js
@@ -8,8 +8,23 @@ do {
     http.open('GET', url, false);
     http.send();
 
-    if (http.status !== 404)
+    if (http.status !== 404) {
         document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\" style=\"padding-top: 20px; display: block; margin: auto; width: 40%;\" /><br>";
+    }
+
+    imageIndex++;
+} while (http.status !== 404);
+
+
+do {
+    let url = "https://cdn.cornhubs.savageavocado.net/images/cornhubs_home_" + imageIndex + ".gif";
+
+    http.open('GET', url, false);
+    http.send();
+
+    if (http.status !== 404) {
+        document.getElementById("center").innerHTML += "<img src=\"" + url + "\" alt=\"corn\" style=\"padding-top: 20px; display: block; margin: auto; width: 40%;\" /><br>";
+    }
 
     imageIndex++;
 } while (http.status !== 404);
